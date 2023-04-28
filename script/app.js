6a08fffcfec5832ab6cc15bc75cfa65191c786be
Giovanni Riefolo <giovanniriefolo@MacBook-Air-di-Giovanni.local>
2023-04-28 07:10:45 +0200

    feat: added Vite, app conf with .env, axios post request

--- script/app.js
+++ script/app.js
@@ -1,7 +1,22 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-jmnXAlX4lh8BpXCBH44Jynb7",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+import axios from "axios"
+
+
+const params = {
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+}
+
+const postText = () => {
+    axios
+        .post('https://api.openai.com/v1/models', params, {
+            headers: {
+                'Authorization': 'Bearer ' + import.meta.env.OPENAI_API_KEY
+                }
+        })
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
+
+postText()
